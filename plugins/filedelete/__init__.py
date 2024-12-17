f414812be42153e57bd696835d5cbef4a60f9a26
guyue2005 <71456735+guyue2005@users.noreply.github.com>
2024-12-18 00:55:17 +0800

    Update __init__.py

--- plugins/filedelete/__init__.py
+++ plugins/filedelete/__init__.py
@@ -17,7 +17,7 @@ class FileDelete(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/guyue2005/MoviePilot-Plugins/main/icons/delete.png"
     # 插件版本
-    plugin_version = "1.4"
+    plugin_version = "1.5"
     # 插件作者
     plugin_author = "guyue2005"
     # 作者主页
