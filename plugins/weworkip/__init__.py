164af88b8ef1bef3f630203115a8660f8f97340f
guyue2005 <71456735+guyue2005@users.noreply.github.com>
2024-12-21 02:18:59 +0800

    Update __init__.py

--- plugins/weworkip/__init__.py
+++ plugins/weworkip/__init__.py
@@ -25,7 +25,7 @@ class WeWorkIP(_PluginBase):
     # 插件名称
     plugin_name = "企微配置IP"
     # 插件描述
-    plugin_desc = "!!Docker用户请使用Docker版!!定时获取最新动态公网IP，配置到企业微信应用的可信IP列表里。"
+    plugin_desc = "【去掉站点认证】!!Docker用户请使用Docker版!!定时获取最新动态公网IP，配置到企业微信应用的可信IP列表里。"
     # 插件图标
     plugin_icon = "https://github.com/suraxiuxiu/MoviePilot-Plugins/blob/main/icons/micon.png?raw=true"
     # 插件版本
